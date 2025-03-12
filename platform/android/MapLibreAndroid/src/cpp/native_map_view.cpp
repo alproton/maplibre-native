a6363bcc7cff94190e8ac4f1af825d08d833b468
siddharthcmdz <siddharth.cmdz@gmail.com>
2025-03-12 08:33:27 -0700

    use color with pre-multiplied alpha for route segments

--- platform/android/MapLibreAndroid/src/cpp/native_map_view.cpp
+++ platform/android/MapLibreAndroid/src/cpp/native_map_view.cpp
@@ -1521,12 +1521,10 @@ jboolean NativeMapView::routeSegmentCreate(JNIEnv& env,
         Result<Color> segmentColorRes = colorConverter(env, color);
         if (segmentColorRes) {
             rsegopts.color = *segmentColorRes;
-            if(rsegopts.color.a < 1.0) {
+            if (rsegopts.color.a < 1.0) {
                 double alpha = rsegopts.color.a;
-                rsegopts.color = mbgl::Color(rsegopts.color.r * alpha,
-                                             rsegopts.color.g * alpha,
-                                             rsegopts.color.b * alpha,
-                                             1.0);
+                rsegopts.color = mbgl::Color(
+                    rsegopts.color.r * alpha, rsegopts.color.g * alpha, rsegopts.color.b * alpha, 1.0);
             }
         }
 
