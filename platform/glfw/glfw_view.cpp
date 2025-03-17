69449c5dc2b139ee95313abb2d9c15eb220f3c7a
siddharthcmdz <siddharth.cmdz@gmail.com>
2025-03-17 09:11:47 -0700

    initial support for imgui

--- platform/glfw/glfw_view.cpp
+++ platform/glfw/glfw_view.cpp
@@ -31,6 +31,12 @@
 #include <mbgl/route/route_manager.hpp>
 #include <mbgl/route/route_segment.hpp>
 #include <random>
+#include "imgui/imgui.h"
+#include "imgui-backends/imgui_impl_glfw.h"
+#include "imgui-backends/imgui_impl_opengl3.h"
+
+#define GL_SILENCE_DEPRECATION
+#define IMGUI_IMPL_OPENGL_ES3
 
 #ifdef _MSC_VER
 #pragma warning(push)
@@ -301,7 +307,22 @@ GLFWView::GLFWView(bool fullscreen_,
 
     pixelRatio = static_cast<float>(backend->getSize().width) / width;
 
+    //Setup imgui
+    if(benchmark) {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsLight();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        const char* glsl_version = "#version 310 es";
+        ImGui_ImplOpenGL3_Init(glsl_version);
+    }
+
     glfwMakeContextCurrent(nullptr);
+
 
     printf("\n");
     printf(
@@ -1480,6 +1501,47 @@ void GLFWView::render() {
 
         report(static_cast<float>(1000 * (glfwGetTime() - started)));
         if (benchmark) {
+
+            // if(glfwGetCurrentContext() == nullptr) {
+                // glfwMakeContextCurrent(window);
+            // }
+            // Start the Dear ImGui frame
+            static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+
+                ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+                ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++;
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+                ImGuiIO& io = ImGui::GetIO(); (void)io;
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                ImGui::End();
+            }
+            // Rendering
+            ImGui::Render();
+            // int display_w, display_h;
+            // glfwGetFramebufferSize(window, &display_w, &display_h);
+            // glViewport(0, 0, display_w, display_h);
+            // glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            // glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            // if(glfwGetCurrentContext() == nullptr) {
+                // glfwMakeContextCurrent(nullptr);
+            // }
+
             invalidate();
         }
     }
@@ -1502,9 +1564,17 @@ void GLFWView::run() {
         {
             MLN_TRACE_ZONE(glfwPollEvents);
             glfwPollEvents();
+            if(benchmark) {
+                if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
+                {
+                    ImGui_ImplGlfw_Sleep(10);
+                }
+            }
         }
 
         render();
+
+
 
 #ifndef __APPLE__
         runLoop.updateTime();
