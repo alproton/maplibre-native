598adee13c33080a8a91eab3a804711ddd1f9892
siddharthcmdz <siddharth.cmdz@gmail.com>
2025-03-25 17:54:35 -0700

    support for better touch intersection with route

--- platform/android/MapLibreAndroid/src/cpp/native_map_view.cpp
+++ platform/android/MapLibreAndroid/src/cpp/native_map_view.cpp
@@ -947,9 +947,15 @@ jni::Local<jni::Array<jlong>> NativeMapView::queryShapeAnnotations(JNIEnv& env, 
     return result;
 }
 
-jint NativeMapView::routeQueryRendered(JNIEnv& env, jni::jdouble screenSpaceX, jni::jdouble screenSpaceY) {
+jint NativeMapView::routeQueryRendered(JNIEnv& env,
+                                       jni::jdouble screenSpaceX,
+                                       jni::jdouble screenSpaceY,
+                                       jni::jint radius) {
     std::vector<RouteID> routeIDs = routeMgr->getAllRoutes();
     std::vector<std::string> layers;
+    // we specifically create caches for base layer since base route is wider than the active layer.
+    // we also check against source name as well as source layer name, since I've seen cases where the source layer name
+    // is not set.
     std::unordered_map<std::string, RouteID> baseLayerMapCache;
     std::unordered_map<std::string, RouteID> baseSourceMapCache;
     for (const auto& routeID : routeIDs) {
@@ -968,20 +974,53 @@ jint NativeMapView::routeQueryRendered(JNIEnv& env, jni::jdouble screenSpaceX, j
         }
     }
 
-    mapbox::geometry::point<double> point = {screenSpaceX, screenSpaceY};
+    std::unordered_map<RouteID, int, IDHasher<RouteID>> routeCoverage;
+    // sample multiple ray picks over an radius
+    for (int i = -radius; i < radius; i++) {
+        for (int j = -radius; j < radius; j++) {
+            mbgl::ScreenCoordinate screenpoint = {screenSpaceX + i, screenSpaceY + j};
+            std::vector<mbgl::Feature> features = rendererFrontend->queryRenderedFeatures(screenpoint, {});
+            for (const auto& feature : features) {
+                if (baseLayerMapCache.find(feature.sourceLayer) != baseLayerMapCache.end()) {
+                    RouteID baseRouteID = baseLayerMapCache[feature.sourceLayer];
+                    routeCoverage[baseRouteID]++;
+                }
 
-    const std::vector<Feature>& features = rendererFrontend->queryRenderedFeatures(point, {layers});
-    for (const auto& feature : features) {
-        if (baseLayerMapCache.find(feature.sourceLayer) != baseLayerMapCache.end()) {
-            return baseLayerMapCache[feature.sourceLayer].id;
-        }
-        // also check cache of geojson source names if the source layer is not set.
-        if (baseSourceMapCache.find(feature.source) != baseSourceMapCache.end()) {
-            return baseSourceMapCache[feature.source].id;
+                // also check cache of geojson source names if the source layer is not set.
+                if (baseSourceMapCache.find(feature.source) != baseSourceMapCache.end()) {
+                    RouteID baseRouteID = baseSourceMapCache[feature.source];
+                    routeCoverage[baseRouteID]++;
+                }
+            }
         }
     }
 
-    return -1;
+    // when you do a touch at a location, the radius can cover multiple routes.
+    // find the RouteID that has the maximum touch weight value
+    int maxTouchWeight = 0;
+    std::vector<RouteID> maxRouteIDs;
+    for (const auto& [routeID, weight] : routeCoverage) {
+        if (weight > maxTouchWeight) {
+            maxTouchWeight = weight;
+        }
+    }
+    for (const auto& [routeID, weight] : routeCoverage) {
+        if (weight == maxTouchWeight) {
+            maxRouteIDs.push_back(routeID);
+        }
+    }
+
+    if (maxRouteIDs.size() == 1) {
+        return maxRouteIDs[0].id;
+    }
+
+    int top = routeMgr->getTopMost(maxRouteIDs);
+    RouteID topRouteID;
+    if (top >= 0 && top < static_cast<int>(maxRouteIDs.size())) {
+        topRouteID = maxRouteIDs[top];
+    }
+
+    return topRouteID.id;
 }
 
 jni::Local<jni::Array<jni::Object<geojson::Feature>>> NativeMapView::queryRenderedFeaturesForPoint(
