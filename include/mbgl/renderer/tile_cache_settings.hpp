b24a3f1ae2e4371bc9bc6b1692a7ae83c17cb84b
alasram <anass.lasram@gmail.com>
2025-05-15 15:10:28 -0700

    Add an option to skip clearing the cache on relayout (#26)

--- include/mbgl/renderer/tile_cache_settings.hpp
+++ include/mbgl/renderer/tile_cache_settings.hpp
@@ -11,6 +11,8 @@ struct TileCacheSettings {
     int maxTiles = 0;             // The computed tile cache size is clamped between minTiles and maxTiles
     bool aggressiveCache = false; // If true, tiles that are not ideal are also cached. Check update_renderables.hpp for
                                   // details about ideal tiles.
+    bool skipRelayoutClear =
+        false; // For certain sources, it is possible to not clear the cache when only geojson causes a tile relayout
 };
 
 using TileCacheSettingsMap = std::unordered_map<std::string, TileCacheSettings>;
