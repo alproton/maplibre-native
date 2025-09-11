e2f9c6086dc69d76271715a08f757b5166b497dc
alasram <anass.lasram@gmail.com>
2025-09-11 08:30:27 -0700

    Add an option to invalidate render tiles

--- include/mbgl/renderer/tile_cache_settings.hpp
+++ include/mbgl/renderer/tile_cache_settings.hpp
@@ -6,12 +6,16 @@
 namespace mbgl {
 
 struct TileCacheSettings {
-    std::string source;           // The tile source ID
-    int minTiles = 0;             // The computed tile cache size is clamped between minTiles and maxTiles
-    int maxTiles = 0;             // The computed tile cache size is clamped between minTiles and maxTiles
-    int minZoom = 0;              // The minimum zoom level for the tile cache
-    int maxZoom = 32;             // The maximum zoom level for the tile cache
-    double cachedTileMaxAge = 0;  // The maximum age of cached tiles in seconds. If 0, no tiles are removed based on age
+    std::string source;          // The tile source ID
+    int minTiles = 0;            // The computed tile cache size is clamped between minTiles and maxTiles
+    int maxTiles = 0;            // The computed tile cache size is clamped between minTiles and maxTiles
+    int minZoom = 0;             // The minimum zoom level for the tile cache
+    int maxZoom = 32;            // The maximum zoom level for the tile cache
+    double cachedTileMaxAge = 0; // The maximum age of cached tiles in seconds. If 0, no tiles are removed based on age
+    double renderTileMaxAge =
+        0; // The maximum age of tiles being rendered in seconds. If 0, no render tiles are removed based on age. The
+           // same render tiles are reused when the camera is not moving. Setting a maximum age ensures short lived
+           // tiles such as rendered traffic tiles are invalidated which forces a cache request.
     bool aggressiveCache = false; // If true, tiles that are not ideal are also cached. Check update_renderables.hpp for
                                   // details about ideal tiles.
     bool skipRelayoutClear =
