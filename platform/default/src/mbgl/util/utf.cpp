0e1534c2a09926fa5797ffa3ca5310622dc6fb69
alasram <anass.lasram@gmail.com>
2025-11-12 15:19:01 -0800

    Log an error if UTF conversion fails (#86)

--- platform/default/src/mbgl/util/utf.cpp
+++ platform/default/src/mbgl/util/utf.cpp
@@ -1,3 +1,4 @@
+#include <mbgl/util/logging.hpp>
 #include <mbgl/util/utf.hpp>
 
 #include <locale>
@@ -7,15 +8,29 @@ namespace mbgl {
 namespace util {
 
 std::u16string convertUTF8ToUTF16(const std::string& str) {
-    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> conv;
-
-    return conv.from_bytes(str);
+    try {
+        std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> conv;
+        return conv.from_bytes(str);
+    } catch (...) {
+        Log::Error(Event::General, "Invalid UTF-8 sequence encountered during conversion to UTF-16:");
+        for (const auto& c : str) {
+            Log::Error(Event::General, std::string(" ") + c);
+        }
+        return u"";
+    }
 }
 
 std::string convertUTF16ToUTF8(const std::u16string& str) {
-    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> conv;
-
-    return conv.to_bytes(str);
+    try {
+        std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> conv;
+        return conv.to_bytes(str);
+    } catch (...) {
+        Log::Error(Event::General, "Invalid UTF-16 sequence encountered during conversion to UTF-8:");
+        for (const auto& c : str) {
+            Log::Error(Event::General, std::string(" ") + static_cast<char>(c));
+        }
+        return "";
+    }
 }
 
 } // namespace util
