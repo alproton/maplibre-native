4b03072e626e4e7b4e4c04079432cbf3a73ba1b9
spalaniappan <spalaniappan@rivianvw.tech>
2026-01-06 21:47:22 -0800

    Revert "remove unused methods causing ANRs"
    This reverts commit 5f5abe9c38bac8a2b87395e04a845ddfda2b7e3f.

--- platform/android/MapLibreAndroid/src/cpp/native_map_view.hpp
+++ platform/android/MapLibreAndroid/src/cpp/native_map_view.hpp
@@ -64,6 +64,10 @@ public:
     void onWillStartLoadingMap() override;
     void onDidFinishLoadingMap() override;
     void onDidFailLoadingMap(MapLoadError, const std::string&) override;
+    void onWillStartRenderingFrame() override;
+    void onDidFinishRenderingFrame(MapObserver::RenderFrameStatus) override;
+    void onWillStartRenderingMap() override;
+    void onDidFinishRenderingMap(MapObserver::RenderMode) override;
     void onDidBecomeIdle() override;
     void onDidFinishLoadingStyle() override;
     void onSourceChanged(mbgl::style::Source&) override;
