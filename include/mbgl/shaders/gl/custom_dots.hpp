3cc3b93909f83d315a4762a7547711d350d9cdc6
spalaniappan <spalaniappan@rivianvw.tech>
2026-03-02 13:11:23 -0800

    support shader version 3.0 instead of 3.1 for emulator

--- include/mbgl/shaders/gl/custom_dots.hpp
+++ include/mbgl/shaders/gl/custom_dots.hpp
@@ -8,9 +8,9 @@ namespace shaders {
 template <>
 struct ShaderSource<BuiltIn::CustomDotsProgram, gfx::Backend::Type::OpenGL> {
     static constexpr const char* name = "CustomDotsProgram";
-    static constexpr const char* vertex = R"(#version 310 es
+    static constexpr const char* vertex = R"(#version 300 es
 layout (location = 0) in vec2 pos;
-layout(location = 1) uniform vec2 size;
+uniform vec2 size;
 out mediump vec2 uv;
 void main() {
   mediump float dx = size.x;
@@ -31,12 +31,12 @@ void main() {
   uv = triangleUv[gl_VertexID];
 }
 )";
-    static constexpr const char* fragment = R"(#version 310 es
+    static constexpr const char* fragment = R"(#version 300 es
 out mediump vec4 fragColor;
 in mediump vec2 uv;
-layout(location = 2) uniform mediump vec3 innerColor;
-layout(location = 3) uniform mediump vec3 outerColor;
-layout(location = 4) uniform mediump float innerFactor;
+uniform mediump vec3 innerColor;
+uniform mediump vec3 outerColor;
+uniform mediump float innerFactor;
 void main() {
   mediump vec2 p = uv * 2.0 - vec2(1.0);
   mediump float outer = 1.0 - min(pow(dot(p, p), 4.0), 1.0);
